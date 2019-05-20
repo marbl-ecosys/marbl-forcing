fe9d84fb1c3119b3cde64d2326d9ced79d4504fe
matt-long <mclong@ucar.edu>
2019-05-20 12:27:00 -0600

    Fe sediment flux computation complete

--- Fe_sediment_flux/config.py
+++ Fe_sediment_flux/config.py
@@ -8,6 +8,8 @@ USER = os.environ['USER']
 DATAROOT = f'/glade/work/{USER}'
 os.environ['DATAROOT'] = DATAROOT
 
+inputdata = '/glade/p/cesmdata/cseg/inputdata'
+
 dirwork = f'{DATAROOT}/cesm_inputdata/work'
 os.makedirs(dirwork, exist_ok=True)   
 
@@ -16,7 +18,6 @@ os.makedirs(dirout, exist_ok=True)
 
 with open('datasets.yaml') as f:
     datasets = yaml.safe_load(f)
-
 
 def compute_vertical_grids():
     vert_grid_edges = {}
@@ -29,10 +30,15 @@ def compute_vertical_grids():
         vert_grid_edges[grid] = depth_edges
         vert_grid_center[grid] = depth_edges[0:-1] + 0.5 * dz
 
-    return vert_grid_edges
+    return vert_grid_edges, vert_grid_center
     
 def sedfrac_file(grid):
     return f'{dirwork}/sedfrac.{grid}.nc'
 
 
-vert_grid_edges = compute_vertical_grids()
+vert_grid_edges, vert_grid_center = compute_vertical_grids()
+
+# TODO: use svn export to get these out of input data repo
+topography_files = {'POP_gx1v7': f'{inputdata}/ocn/pop/gx1v7/grid/topography_20161215.ieeei4', 
+                    'POP_gx3v7': f'{inputdata}/ocn/pop/gx3v7/grid/topography_20100105.ieeei4',
+                    'POP_tx0.1v3': f'{inputdata}/ocn/pop/tx0.1v3/grid/topography_20170718.ieeei4'}
